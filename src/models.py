919651b58f54b0eb1aebf1c952df67fa9410ca48
Jashim <jashim.vanover@gmail.com>
2023-12-09 15:39:07 -0500

    model and movie

--- src/models.py
+++ src/models.py
@@ -1,1 +1,9 @@
 # TODO - Create SQLAlchemy DB and Movie model
+from app import db
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    director = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -2,18 +2,43 @@ class MovieRepository:
 
     def get_all_movies(self):
         # TODO get all movies from the DB
-        return None
+
+        from app import db
+        from app.models import Movie
+
+        class MovieRepository:
+            def get_all_movies(self):
+                return db.session.query(Movie).all()
+                return None
 
     def get_movie_by_id(self, movie_id):
         # TODO get a single movie from the DB using the ID
-        return None
+        from app import db
+        from app.models import Movie
+
+        class MovieRepository:
+            def get_movie_by_id(self, movie_id):
+                return db.session.query(Movie).filter_by(movie_id=movie_id).first()
+                return None
 
     def create_movie(self, title, director, rating):
         # TODO create a new movie in the DB
-        return None
+        from app import db
+        from app.models import Movie
+
+        class MovieRepository:
+            def create_movie(self, title, director, rating):
+                new_movie = Movie(title=title, director=director, rating=rating)
+                db.session.add(new_movie)
+                db.session.commit()
+                return new_movie
+                return None
 
     def search_movies(self, title):
         # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
+        from app import db
+        from app
+
         return None
 
 
